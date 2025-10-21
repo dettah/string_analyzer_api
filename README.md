cd8f50a597691f8462fe9b31645a791d108c9544
dettah <dettah7@gmail.com>
2025-10-21 15:02:31 +0100

    Add gunicorn and fix deployment config

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# String Analyzer API
+# String Analyzer API (Django REST Framework)
+
+### Description
+REST API that analyzes strings and stores their computed properties:
+- Length
+- Palindrome status
+- Unique characters
+- Word count
+- SHA-256 hash
+- Character frequency map
+
+### Stack
+- Python 3.x
+- Django
+- Django REST Framework
+
+### How to Run Locally
+```bash
+git clone https://github.com/dettah/string_analyzer_api.git
+cd string_analyzer
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py runserver
--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o7*#uld$$c)*uf2git_0p5r&q3=0(fb11gnk6bgjd2c4s!e947'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
